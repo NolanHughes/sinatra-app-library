81fd16109b4f3424714dfd06dfc1a29578435d0b
NolanHughes <Nhughes987@gmail.com>
2018-02-03 16:53:23 -0600

    Create post route for login page. Displays user show page if valid input, returns error message and login page if not.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,4 +6,26 @@ class UserController < AppController
   get '/users/signup' do
     erb :'/users/signup'
   end
+
+  post '/users/login' do
+    # binding.pry
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/users/show'
+    else
+      flash[:message] = "*Please enter a valid username and password"
+      erb :'/users/login'
+    end
+  end
+
+  post '/users/signup' do
+    binding.pry
+  end
+
+  get '/users/show' do
+    #make current user method
+    erb :'/users/show'
+  end
 end
