04a58d53e432b8e82d610878ec03ba87c84c9ef5
NolanHughes <Nhughes987@gmail.com>
2018-02-03 16:31:15 -0600

    Create route and erb file for homepage with links to login and signup. Create routes and pages for login and signup

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -2,7 +2,16 @@ require './config/environment'
 require 'rack-flash'
 
 class AppController < Sinatra::Base
+  use Rack::Flash
+
+  configure do
+    # set :public_folder, 'public'#Not sure what this does
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
   get '/' do
-    "Hello World"
+    erb :'index'
   end
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,9 @@
+class UserController < AppController
+  get '/users/login' do
+    "Hello!"
+  end
+
+  get '/users/signup' do
+    "Hello again!"
+  end
+end
