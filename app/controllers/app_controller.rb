5bc20b57913c387bce52786a35d70e4121c4b04b
NolanHughes <Nhughes987@gmail.com>
2018-02-08 09:15:10 -0600

    Use links to bootstrap instead of having hte files in my public folder.

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -5,7 +5,7 @@ class AppController < Sinatra::Base
   use Rack::Flash
 
   configure do
-    # set :public_folder, 'public'#Not sure what this does. Maybe has to do with public folder for css, js, etc.
+    set :public_folder, 'public'#Not sure what this does. Maybe has to do with public folder for css, js, etc.
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "secret"
@@ -14,5 +14,5 @@ class AppController < Sinatra::Base
   get '/' do
     erb :'index'
   end
-  
+
 end
